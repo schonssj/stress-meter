def3737c5153f284cab99f96beab72383d1ab118
schonssj <jvschons@gmail.com>
2025-05-19 22:59:21 -0300

    refactor: modularize navigation

--- App.tsx
+++ App.tsx
@@ -1,31 +1,10 @@
-import Home from './src/pages/Home';
-import Result from './src/pages/Result';
-import Question from './src/pages/Question';
+import AppNavigator from './src/navigation/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name='Home'
-          component={Home}
-          options={{ title: 'Teste do Estresse', headerStyle: { backgroundColor: 'teal'}, headerTitleStyle: { color: 'white' } }}
-        />
-        <Stack.Screen 
-          name='Question'
-          component={Question}
-          options={{ title: 'Teste do Estresse', headerStyle: { backgroundColor: 'teal'}, headerTitleStyle: { color: 'white' } }}
-        />
-        <Stack.Screen 
-          name='Result'
-          component={Result}
-          options={{ title: 'Teste do Estresse', headerStyle: { backgroundColor: 'teal'}, headerTitleStyle: { color: 'white' } }}
-        />
-      </Stack.Navigator>
+      <AppNavigator />
     </NavigationContainer>
   );
 }
---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,28 @@
+import Home from '../pages/Home';
+import Result from '../pages/Result';
+import Question from '../pages/Question';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator()
+
+const AppNavigator = () => (
+    <Stack.Navigator>
+        <Stack.Screen 
+          name='Home'
+          component={Home}
+          options={{ title: 'Teste do Estresse', headerStyle: { backgroundColor: 'teal'}, headerTitleStyle: { color: 'white' } }}
+        />
+        <Stack.Screen 
+          name='Question'
+          component={Question}
+          options={{ title: 'Teste do Estresse', headerStyle: { backgroundColor: 'teal'}, headerTitleStyle: { color: 'white' } }}
+        />
+        <Stack.Screen 
+          name='Result'
+          component={Result}
+          options={{ title: 'Teste do Estresse', headerStyle: { backgroundColor: 'teal'}, headerTitleStyle: { color: 'white' } }}
+        />
+    </Stack.Navigator>
+);
+
+export default AppNavigator;
